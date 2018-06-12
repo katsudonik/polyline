1666958a3d785c53a4d48076e2a0351d74f99044
katsudonik <23866351+katsudonik@users.noreply.github.com>
2018-06-12 11:55:05 +0900

    .

--- screenshot.py
+++ screenshot.py
@@ -3,5 +3,5 @@ from selenium import webdriver
 driver = webdriver.PhantomJS()
 driver.set_window_size(1024, 768)
 driver.get('http://localhost:80/cgi-bin/polyline.py')
-driver.save_screenshot('polyline.png')
+driver.save_screenshot('/var/www/html/polyline.png')
 driver.close()
