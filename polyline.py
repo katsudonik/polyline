d0761b47311f63667681965bbaaada9cda3b9707
root <root@localhost.localdomain>
2018-06-12 05:30:34 +0000

    centering

--- polyline.py
+++ polyline.py
@@ -3,12 +3,19 @@
 print("Content-Type: text/html\n")
 
 import folium
-points = []
-points.append(tuple([35.658505, 139.802043]))
-points.append(tuple([35.660445, 139.800450]))
-points.append(tuple([35.661617, 139.802349]))
+import numpy as np
 
-m = folium.Map(location=[35.660445, 139.800450], zoom_start=15)
+points = []
+points.append([35.658505, 139.802043])
+points.append([35.661617, 139.802349])
+points.append([35.660445, 139.800450])
+
+points = np.array(points)
+location = [( np.max(points[:,0]) + np.min(points[:,0]) ) / 2, ( np.max(points[:,1]) + np.min(points[:,1]) ) / 2]
+
+points = points.tolist()
+
+m = folium.Map(location=location, zoom_start=15)
 folium.PolyLine(points).add_to(m)
 folium.Marker(points[0], popup='Mt. Hood Meadows').add_to(m)
 folium.Marker(points[1], popup='Timberline Lodge').add_to(m)
