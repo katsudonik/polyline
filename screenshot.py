cd5000f03b1eb9bf378cbc44f1f3b8396f302a9d
ik <katsudonwork@gmail.com>
2018-08-27 07:11:28 +0000

    compress

--- screenshot.py
+++ screenshot.py
@@ -1,7 +1,16 @@
 from selenium import webdriver
+import cv2
 
 driver = webdriver.PhantomJS()
 driver.set_window_size(1922, 1922)
 driver.get('http://localhost:80/cgi-bin/polyline/polyline.py')
 driver.save_screenshot('/var/www/html/polyline.png')
 driver.close()
+
+
+
+rate = 9
+compression_params = [int(cv2.IMWRITE_PNG_COMPRESSION), rate]
+img = cv2.imread('/var/www/html/polyline.png', cv2.IMREAD_UNCHANGED)
+#cv2.imwrite('/var/www/html/polyline' + str(rate) + '.png', img, compression_params)
+cv2.imwrite('/var/www/html/polyline.png', img, compression_params)
